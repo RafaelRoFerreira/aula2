67b196b81c35c6259a8b7b56a61c19d1306a3e32
rafael <rafael123>
2026-06-02 09:11:48 -0400

    comimit inicial

---
+++ o.py
@@ -0,0 +1,12 @@
+idade = int(input("em que ano voce nasceu?:"))
+
+if idade >=1910 and idade <= 2007:
+    print ("voce é MAIOR de idade")
+
+elif idade >2007:
+    print("voce é menor de idade")
+
+elif idade < 1910:
+    print("voce ja morreu kkkkkk")
+else:
+    print("tu nem nasceu aindakkkkk")
